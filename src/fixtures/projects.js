c4ea20056db793575abe40a4325a5b0a16013f7c
James Ortiz <jameswortiz728@gmail.com>
2023-05-16 21:11:31 -0400

    Rearranged projects

--- src/fixtures/projects.js
+++ src/fixtures/projects.js
@@ -1,13 +1,13 @@
 export default [{
-    title: 'magic-conch',
-    name: 'The Magic Conch',
-    listDescription: 'Put your life in the hands of a computer!',
-    pageDescription: 'Put your life in the hands of a computer! Created using React.js, and styled with CSS',
-    url: 'https://magic-conch.herokuapp.com/'
-}, {
     title: 'expenses-tracker',
     name: 'Expenses Tracker',
     listDescription: 'Take control of your expenses!',
     pageDescription: 'Take control of your expenses! Created using React.js, styled with CSS, and connected to Google Firebase',
     url: 'https://expenses-tracker-117.herokuapp.com/'
+}, {
+    title: 'magic-conch',
+    name: 'The Magic Conch',
+    listDescription: 'Put your life in the hands of a computer!',
+    pageDescription: 'Put your life in the hands of a computer! Created using React.js, and styled with CSS',
+    url: 'https://magic-conch.herokuapp.com/'
 }];
