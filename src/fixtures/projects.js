60472018669a20cbe12c16c57a4622c1ec84940e
James Ortiz <jameswortiz728@gmail.com>
2023-05-22 20:19:38 -0400

    Changed links to reflect new host domain

--- src/fixtures/projects.js
+++ src/fixtures/projects.js
@@ -3,20 +3,20 @@ export default [{
     name: 'JWOrtiz.dev',
     listDescription: "You're already here! Features: React, SCSS",
     pageDescription: 'Personal Portfolio Site. Created using React, CSS, Heroku',
-    liveSite: 'https://expenses-tracker-117.herokuapp.com/',
+    liveSite: 'https://www.jwortiz.dev/',
     sourceCode: 'https://github.com/jameswortiz728/jwortiz.dev'
 }, {
     title: 'expenses-tracker',
     name: 'Expenses Tracker',
     listDescription: 'Take control of your expenses! Features: React, Redux, Firebase, SCSS, Heroku',
     pageDescription: 'Take control of your expenses! Created using React.js, State manage by Redux, styled with CSS, integrated test suite using Jest, connected to Google Firebase, hosted on Heroku',
-    liveSite: 'https://expenses-tracker-117.herokuapp.com/',
+    liveSite: 'https://expenses-tracker.jwortiz.dev/',
     sourceCode: 'https://github.com/jameswortiz728/Expenses-Tracker'
 }, {
     title: 'magic-conch',
     name: 'The Magic Conch',
     listDescription: 'Put your life in the hands of a computer! Features: React, SCSS, Heroku',
     pageDescription: 'Put your life in the hands of a computer! Created using React.js, styled with CSS, hosted on Heroku',
-    liveSite: 'https://magic-conch.herokuapp.com/',
+    liveSite: 'https://magic-conch.jwortiz.dev/',
     sourceCode: 'https://github.com/jameswortiz728/magic-conch'
 }];
