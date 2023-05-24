8b97aef94d5b10db330341f6a28f05da6aa56fac
James Ortiz <jameswortiz728@gmail.com>
2023-05-24 16:34:38 -0400

    Updated contact form to React componenent

--- src/components/ContactMePage.js
+++ src/components/ContactMePage.js
@@ -1,31 +1,52 @@
 import React from 'react';
+import { useForm, ValidationError } from '@formspree/react';
 
-const ContactMePage = () => {
-    return (
+const ContactMePage = ()=> {
+  const [state, handleSubmit] = useForm("mlekkjze");
+
+  if (state.succeeded) {
+      return <p>Thanks for joining!</p>;
+  }
+
+  return (
         <div id="contactme" className="spacer">
             <div className="page-header__main">
                 <div className="content-container">
                     <h1 className="page-header__title">Contact Me</h1>
+                    <p>Fill out the below form to send me an email and get in contact</p>
                 </div>
             </div>
             <div className="content-container">
-                <form
-                    action="https://formspree.io/f/mlekkjze"
-                    method="POST"
-                >
-                    <label>
-                        Your email:
-                        <input type="email" name="email"/>
+                <form onSubmit={handleSubmit}>
+                    <label htmlFor="email">
+                        Email Address
                     </label>
-                    <label>
-                        Your message:
-                        <textarea name="message"></textarea>
-                    </label>  
-                    <button type="submit">Send</button>
+                    <input
+                        id="email"
+                        type="email" 
+                        name="email"
+                    />
+                    <ValidationError 
+                        prefix="Email" 
+                        field="email"
+                        errors={state.errors}
+                    />
+                    <textarea
+                        id="message"
+                        name="message"
+                    />
+                    <ValidationError 
+                        prefix="Message" 
+                        field="message"
+                        errors={state.errors}
+                    />
+                    <button type="submit" disabled={state.submitting}>
+                        Submit
+                    </button>
                 </form>
             </div>
         </div>
-    )
+    );
 }
 
 export default ContactMePage;
