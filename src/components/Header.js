b88046ddf45eed13511273d783718d871d5269e3
James Ortiz <jameswortiz728@gmail.com>
2023-05-25 21:09:50 -0400

    Clicking on Logo will now close mobile dropdown

--- src/components/Header.js
+++ src/components/Header.js
@@ -23,7 +23,7 @@ export const Header = () => {
             <div className="content-container">
                 <div className="header__content">
                     <Link className="header__title" to="/" onClick={() => handleScrollTo('dashboard')}>
-                        <h1>JWOrtiz.dev</h1>
+                        <h1 onClick={closeDropdown}>JWOrtiz.dev</h1>
                     </Link>
                     <Link className="show-for-desktop" to="/" state = {{ id: "aboutMe" }}><button onClick={() => handleScrollTo('aboutMe')} className="button button--link">About Me</button></Link>
                     <Link className="show-for-desktop" to="/" state = {{ id: "projects" }}><button onClick={() => handleScrollTo('projects')} className="button button--link">Projects</button></Link>
