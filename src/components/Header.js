6e595d62a85d5af8bd49d48bbd255472b0869191
James Ortiz <jameswortiz728@gmail.com>
2023-05-25 20:23:40 -0400

    Added dropdown for mobile navigation

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,21 +1,51 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import handleScrollTo from '../functions/handleScrollTo';
 
-export const Header = () => (
-    <header className="header">
-        <div className="content-container">
-            <div className="header__content">
-                <Link className="header__title" to="/" onClick={() => handleScrollTo('dashboard')}>
-                    <h1>JWOrtiz.dev</h1>
-                </Link>
-                <Link to="/" state = {{ id: "aboutMe" }}><button onClick={() => handleScrollTo('aboutMe')} className="button button--link">About Me</button></Link>
-                <Link to="/" state = {{ id: "projects" }}><button onClick={() => handleScrollTo('projects')} className="button button--link">Projects</button></Link>
-                <Link to="/" state = {{ id: "resume" }}><button onClick={() => handleScrollTo('resume')} className="button button--link">Resume</button></Link>
-                <Link to="/" state = {{ id: "contactme" }}><button onClick={() => handleScrollTo('contactme')} className="button button--link">Contact Me</button></Link>
+export const Header = () => {
+
+    const [open, setOpen] = useState(false);
+
+    const openDropdown = () => {
+        setOpen(true);
+    };
+
+    const closeDropdown = () => {
+        setOpen(false);
+    };
+
+    const switchDropdown = () => {
+        setOpen(!open);
+    };
+
+    return (
+        <header className="header">
+            <div className="content-container">
+                <div className="header__content">
+                    <Link className="header__title" to="/" onClick={() => handleScrollTo('dashboard')}>
+                        <h1>JWOrtiz.dev</h1>
+                    </Link>
+                    <Link className="show-for-desktop" to="/" state = {{ id: "aboutMe" }}><button onClick={() => handleScrollTo('aboutMe')} className="button button--link">About Me</button></Link>
+                    <Link className="show-for-desktop" to="/" state = {{ id: "projects" }}><button onClick={() => handleScrollTo('projects')} className="button button--link">Projects</button></Link>
+                    <Link className="show-for-desktop" to="/" state = {{ id: "resume" }}><button onClick={() => handleScrollTo('resume')} className="button button--link">Resume</button></Link>
+                    <Link className="show-for-desktop" to="/" state = {{ id: "contactme" }}><button onClick={() => handleScrollTo('contactme')} className="button button--link">Contact Me</button></Link>
+                    <div className="show-for-mobile">
+                        <button onClick={switchDropdown} className="button button--link">Fast Travel</button>
+                    </div>
+                </div>
+                {open && 
+                    <div className="show-for-mobile">
+                        <ul>
+                            <li><Link onClick={closeDropdown} to="/" state = {{ id: "aboutMe" }}><button onClick={() => handleScrollTo('aboutMe')} className="button button--link">About Me</button></Link></li>
+                            <li><Link onClick={closeDropdown} to="/" state = {{ id: "projects" }}><button onClick={() => handleScrollTo('projects')} className="button button--link">Projects</button></Link></li>
+                            <li><Link onClick={closeDropdown} to="/" state = {{ id: "resume" }}><button onClick={() => handleScrollTo('resume')} className="button button--link">Resume</button></Link></li>
+                            <li><Link onClick={closeDropdown} to="/" state = {{ id: "contactme" }}><button onClick={() => handleScrollTo('contactme')} className="button button--link">Contact Me</button></Link></li>
+                        </ul>
+                    </div>
+                }
             </div>
-        </div>
-    </header>
-);
+        </header>
+    )
+};
 
 export default Header;
