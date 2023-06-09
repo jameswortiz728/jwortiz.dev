7a31dd74eeedfd5afa8c0c8bf2457eb05fcd4b65
James Ortiz <jameswortiz728@gmail.com>
2023-06-09 17:14:48 -0400

    Updated project descriptions

--- src/fixtures/projects.js
+++ src/fixtures/projects.js
@@ -2,21 +2,21 @@ export default [{
     title: 'jwortiz-dev',
     name: 'JWOrtiz.dev',
     listDescription: "You're already here! Features: React, SCSS",
-    pageDescription: 'Personal Portfolio Site. Created using React, CSS, Mobile friendly, Heroku',
+    pageDescription: "You're already here! Personal Portfolio Site. Mobile Friendly. Created using React.js, styled with SCSS.",
     liveSite: 'https://www.jwortiz.dev/',
     sourceCode: 'https://github.com/jameswortiz728/jwortiz.dev'
 }, {
     title: 'expenses-tracker',
     name: 'Expenses Tracker',
-    listDescription: 'Take control of your expenses! Features: React, Redux, Firebase, SCSS, Heroku',
-    pageDescription: 'Take control of your expenses! Created using React.js, State managed by Redux, styled with CSS, integrated test suite using Jest, connected to Google Firebase, Mobile friendly, hosted on Heroku',
+    listDescription: 'Take control of your expenses! Features: React, Redux, Firebase, SCSS',
+    pageDescription: 'Take control of your expenses! Add, Edit, or Remove expenses. Search for expenses using filters based on Amount, Date created, and others. Mobile Friendly. Created using React.js, state managed by Redux, styled with SCSS, tested with Jest, connected to Google Firebase.',
     liveSite: 'https://expenses-tracker.jwortiz.dev/',
     sourceCode: 'https://github.com/jameswortiz728/Expenses-Tracker'
 }, {
     title: 'magic-conch',
     name: 'The Magic Conch',
-    listDescription: 'Put your life in the hands of a computer! Features: React, SCSS, Heroku',
-    pageDescription: 'Put your life in the hands of a computer! Created using React.js, styled with CSS, hosted on Heroku',
+    listDescription: 'Too many choices? Let the Magic Conch decide for you. Features: React, SCSS',
+    pageDescription: 'Too many choices? Let the Magic Conch decide for you. Mobile Friendly. Created using React.js, styled with SCSS.',
     liveSite: 'https://magic-conch.jwortiz.dev/',
     sourceCode: 'https://github.com/jameswortiz728/magic-conch'
 }];
