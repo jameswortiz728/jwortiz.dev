697984c35db7f8ebc36aad60f2cdfa2907f3f4cc
James Ortiz <jameswortiz728@gmail.com>
2023-06-14 14:50:09 -0400

    Updated resume and project descriptions

--- src/fixtures/projects.js
+++ src/fixtures/projects.js
@@ -8,15 +8,15 @@ export default [{
 }, {
     title: 'jwortiz-dev',
     name: 'JWOrtiz.dev',
-    listDescription: "You're already here! Features: React, SCSS",
-    pageDescription: "You're already here! Personal portfolio site. Mobile Friendly. Created using React.js, styled with SCSS.",
+    listDescription: "You're already here! Features: React, SCSS, Mobile friendly",
+    pageDescription: "You're already here! Personal portfolio site. Mobile friendly. Created using React.js, styled with SCSS.",
     liveSite: 'https://www.jwortiz.dev/',
     sourceCode: 'https://github.com/jameswortiz728/jwortiz.dev'
 }, {
     title: 'expenses-tracker',
     name: 'Expenses Tracker',
-    listDescription: 'Take control of your expenses! Features: React, Redux, Firebase, SCSS',
-    pageDescription: 'Take control of your expenses! Add, edit, or remove expenses. Search for expenses using filters based on amount, date created, and others. Mobile Friendly. Created using React.js, state managed by Redux, styled with SCSS, tested with Jest, connected to Google Firebase.',
+    listDescription: 'Take control of your expenses! Features: React, Redux, Firebase, SCSS, Mobile friendly',
+    pageDescription: 'Take control of your expenses! Add, edit, or remove expenses. Search for expenses using filters based on amount, date created, and others. Mobile friendly. Created using React.js, state managed by Redux, styled with SCSS, tested with Jest, connected to Google Firebase.',
     liveSite: 'https://expenses-tracker.jwortiz.dev/',
     sourceCode: 'https://github.com/jameswortiz728/Expenses-Tracker'
 }];
